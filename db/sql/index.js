9b19a3492897219bfa03b03190d2763a12cf86a9
mfosho <mc8160@gmail.com>
2018-10-21 18:56:27 -0400

    Created database, filter by genre, and search

--- db/sql/index.js
+++ db/sql/index.js
@@ -2,3 +2,14 @@ const mysql = require('mysql');
 const mysqlConfig = require('../../config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+})
+
+module.exports.connection = connection;
--- db/sql/moviesSchema.sql
+++ db/sql/moviesSchema.sql
@@ -1,1 +1,13 @@
 -- SET UP SCHEMA HERE
+-- DROP DATABASE movies;
+CREATE DATABASE IF NOT EXISTS movies;
+USE movies;
+
+CREATE TABLE IF NOT EXISTS movie (
+  id            INT NOT NULL AUTO_INCREMENT,
+  movie_name    VARCHAR(20) NOT NULL,
+  genre         VARCHAR(20),
+  rating        INT,
+  PRIMARY KEY (id)
+);
+
